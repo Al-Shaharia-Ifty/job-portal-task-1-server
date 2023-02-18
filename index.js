b49806832eba1b39ccbcea4944419e1d02da9234
Al Shaharia <ifty682002@gmail.com>
2023-02-18 14:05:47 +0600

    freasher and expriencde are done

--- index.js
+++ index.js
@@ -7,14 +7,35 @@ const Port = process.env.Port || 5000;
 
 // Get data
 const FresherJobs = require("./Data/FresherJob.json");
+const ExperiencedJobs = require("./Data/ExperiencedJob.json");
 
 //
 app.get("/", (req, res) => {
   res.send("Now server is running");
 });
 
+// get fresherJobs
 app.get("/fresherJobs", (req, res) => {
   res.send(FresherJobs);
+});
+
+// get fresherJob by id
+app.get("/fresher/:id", (req, res) => {
+  const id = req.params.id;
+  const result = FresherJobs.find((f) => f.id === id);
+  res.send(result);
+});
+
+// get experienced Jobs
+app.get("/experiencedJobs", async (req, res) => {
+  res.send(ExperiencedJobs);
+});
+
+// get fresherJob by id
+app.get("/experienced/:id", (req, res) => {
+  const id = req.params.id;
+  const result = ExperiencedJobs.find((f) => f.id === id);
+  res.send(result);
 });
 
 //
